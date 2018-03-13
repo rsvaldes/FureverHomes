454c8c273373cdbb116c93954e566a21d8cec05c
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2018-03-13 13:42:04 -0600

    added bootstrap

--- fureverhomes/src/components/Home.jsx
+++ fureverhomes/src/components/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import '../App.css';
+// import '../App.css';
 import Search from './Search';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const Home = () => (
-  <p>Welcome to furever homes. Sroll down to learn more!</p>
+  <div className="container">
+<div className="jumbotron bg-info">
+<h1 className="display-3">Welcome To Furever Homes!</h1>
+<p className="lead">Continue scrolling to learn more</p>
+<hr className="my-4" />
+<p className="lead">
+<a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</p>
+</div>
+</div>
 )
 
 // const HomePage = () => <div>This is a Home Page</div>
--- fureverhomes/src/index.js
+++ fureverhomes/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'font-awesome/css/font-awesome.css';
+ReactDOM.render(<App />, document.getElementById('hello'));
 registerServiceWorker();
